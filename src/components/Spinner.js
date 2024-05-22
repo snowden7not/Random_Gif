0b9ff253df8dd398d11f9bbc167ca211110f231c
Lakhan Kumar <109954175+snowden7not@users.noreply.github.com>
2024-05-22 19:25:46 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import loader from "./infinite-spinner.svg";
 const Spinner=()=>{
     return (
         <div>
-            <img src={loader} width="100" height="100" alt="loading wala spinner"/>
+            <img src={loader} width="100" height="100" alt=""/>
         </div>
     )
 }
